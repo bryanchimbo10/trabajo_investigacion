65450d7a0b4acd11d6a98f00c6aab776ad60c0f5
bryanchimbo10 <133473268+bryanchimbo10@users.noreply.github.com>
2024-05-28 19:49:21 -0500

    Add files via upload

--- Menu.js
+++ Menu.js
@@ -1,19 +1,15 @@
 const read = require (`prompt-sync`)();
-
 function menu (){
     let menu=0
     menu=read(`Ingrese un numero del 1-24:  `)
     switch( menu){
-        case "1":
-            
+        case "1": 
           function ejercicio1(){
+            console.log("1.	Algoritmo que transforme de grados Celsius a Fahrenheit")
             let var2=32, celsius=0, fahren=0
             let var3=1.8 
-    
-
             celsius = read ("Ingrese los grados celcius: ")
             celsius= parseFloat(celsius)
-
             fahren= celsius*var3+var2
             var3=parseFloat(var3)
             var2= parseInt(var2)
@@ -23,23 +19,21 @@ function menu (){
            ejercicio1()
         break;
         case "2":
-            
          function ejercicio2 (){
+          console.log("2.	Algoritmo que lea un numero entero, obtenga y presente el doble y el triple del ")
          let num1=0, doble=0,triple=0
-         num1= read ("ingrese la cantidad para calcular su doble: ")
+         num1= read ("ingrese la cantidad para calcular su doble y el triple: ")
          num1 = parseInt(num1)
-
          doble= num1+num1
          triple=num1+num1+num1
-     
          console.log (`El doble de tu cantidad ingresada es de: `,doble )
          console.log (`El triple de tu cantidad ingresada es de: `,triple )
          }
-
           ejercicio2()
         break
         case "3":
             function ejercicio3 (){
+              console.log("3.	Diseñar un Algoritmo que lea 4 variables y calcule e imprima su producto, su suma y su media aritmética")
                 let num1=0, num2=0,num3=0,num4=0,suma=0, media=0
                 num1= read ("ingrese una cantidad: ") 
                 num1 = parseInt(num1)
@@ -53,28 +47,47 @@ function menu (){
                 media=suma/4
                 console.log(`la suma de las cantidades es de ${suma}  la media es de ${media} `)
             }
-            
             ejercicio3()
         break;
         case "4":
             function ejercicio4 (){
+              console.log("")
                 let pl=0, k=0, g=0
                 pl= read ("ingrese el peso en libras : ")
                 pl = parseInt(pl)
-            
                 k= pl/2.20
                 g=pl/100
-                 
                 console.log (`El peso en kilogramos es: `,k)
                 console.log (`El peso en gramos es: `,g )
             }
-            
             ejercicio4()
             break;
             case "5":
+                function ejercicio5() {
+                    console.log("5.	Diseñar un algoritmo que resuelva la siguiente expresión matemática:") 
+                    console.log("x=((sen(a)+cos(b))*(trunc(a) mod 2))+(raiz(a^3)/(a*b+c))")
+                    let parte1=0, parte2=0, x=0, valora=0, valorb=0,valorc=0
+                    const sin = Math.sin
+                    const cos = Math.cos
+                    const sqrt = Math.sqrt
+                    const trunc = Math.trunc
+                    const pow = Math.pow
+                     valora= read (`ingrese el valor de A: `)
+                     valora=parseInt(valora)
+                     valorb= read (`ingrese el valor de B: `)
+                     valorb=parseInt(valorb)
+                     valorc= read (`ingrese el valor de C: `)
+                     valorc=parseInt(valorc)
+                      parte1 = (sin(valora) + cos(valorb)) * (trunc(valorc) % 2)
+                      parte2 = sqrt(pow(valora, 3)) / (valora* valorb + valorc)
+                      x = parte1 + parte2;
+                     console.log(`el resultado es: ${x}`)
+                 }
+                 ejercicio5()  
             break;
             case "6":
                 function ejercicio6 (){
+                  //console.log("6.	Un empleado trabaja 40 horas en jornada normal en la semana a razón $5 la hora. Si trabaja más de 40 horas, estas serán considerados horas de sobretiempo que se pagan al doble de la hora de la jornada normal. El porcentaje del seguro social(iess) es del 10% del ingreso total. Se desea saber cuál es el valor del sueldo, sobretiempo, ingreso total, seguro social y el neto a recibir.")
                     let horast=0,horasextras=0,ss=0.10,sobretiempo=0,ingtotal=0,ss2=0,sn=0,sueldosemana=0,horasextras2=0
                      horast= read(`Ingrese las horas de trabajo:`)
                      horast=parseInt(horast)
@@ -94,12 +107,12 @@ function menu (){
                         sueldosemana=horast*5
                         console.log(`Usted no cuenta con hora extras su sueldo de la semana es: ${sueldosemana}`)
                      }
-                     
                 }
                 ejercicio6()
             break;
             case "7":
                 function ejercicio7 (){
+                  //console.log("7.	Algoritmo que pida dos números y presenta el mayor de los dos siempre y cando el primero sea par y el segundo impar")
                     let num1=0, num2=0
                     num1= read ("ingrese una cantidad: ") 
                     num1 = parseInt(num1)
@@ -117,11 +130,11 @@ function menu (){
                         console.log(`No se cumple con la primera condicion `)
                     }
                 }
-                
                 ejercicio7()
             break;
             case "8":
                 function ejercicio8(){
+                  console.log("8.	Leer un carácter y deducir si está o no comprendido entre las letras a y z ambas inclusive y sino verificar si es un signo de puntuacion , . ; :y si no presentar solo el carácter.")
                     let arreglo4=[]
                     let numero4=0
                     numero4 = read ('ingrese un valor:')
@@ -136,21 +149,16 @@ function menu (){
                     }
                     else{
                       console.log ( "no cumple las condiciones")
-                    }
-                    
-                    
-                    
+                    }  
                 }
                 ejercicio8()
-                  
             break;
             case "9":
                 function ejercicio9(){
+                  console.log("9.	Determinar cuánto se  debe pagar por x  cantidad de colas, considerando que si  son menos de 12 colas, el costo por unidad es de $0,25 caso  contrario el precio será  10% menos.")
                     let cc=0, ds=0.10,dcc=0,cu=0.25,cc2=0,cu2=0 
                     cc= read ("ingrese la cantidad de colas adquiridas: ")
                     cc = parseInt(cc)
-                
-                
                     if (cc<12){
                         cc=cc*cu
                         console.log(` el valor a cancelar por su compra es de ${cc}`)
@@ -160,15 +168,13 @@ function menu (){
                        cu2=cc*cc2
                 
                        console.log(`el valor por unidad con el descuento es ${cc2}, el valor a cancelar por su compra es de ${cu2}`)
-                
                     }
-                
                 }
-                
                 ejercicio9()
             break;
             case "10":
                 function ejercicio10 (){
+                 // console.log("El almacén “AlgoritmoX” tiene una promoción: a todos los trajes que tienen un precio superior a 200, se les aplicará un descuento del 10% y al resto tendrán un descuento de solo $10 dólares. Presentar el valor de cada traje con su respectivo valor, descuento y pago considerando el iva del 15%.")
                     let valortraje=0, descuento=0.10,descuento2=10,valordescuento=0,valordescuento3=0,iva=0.15,iva1=0,iva2=0
                     valortraje= read ("ingrese el valor del traje: ")
                     valortraje = parseInt(valortraje)
@@ -179,27 +185,22 @@ function menu (){
                         iva1=valordescuento3*iva
                         iva2=valordescuento+iva
                         console.log(`El valo incial de su compra es de ${valortraje},  el valor de se traje con descuento es ${valordescuento3}, el precio finala cancelar con iva de 15% es de ${iva2}`)
-                       // console.log(`${valordescuento} ${valordescuento3} ${iva1} ${iva2}`)
                       
                     }else {
                         valordescuento=valortraje-descuento2
                         iva1=valordescuento*iva 
                         iva2=valordescuento+iva
                         console.log(`El valo incial de su compra es de ${valortraje},  el valor de se traje con descuento es ${valordescuento}, el precio finala cancelar con iva de 15% es de ${iva2}`)
-                    }
-                    
+                    }  
                 }
-                
                 ejercicio10()
             break;
             case "11":
                 function ejercicio11(){
-                  
+                  console.log("Dado un día en numero, presentar el nombre de ese día. Para la solución utilice arreglos.")
                     let diasemana=["lunes","martes","miercoles","jueves","viernes","sabado","domingo"]
                     let ds=""
-                  
                     ds= read(`ingrese un numero compredido entre el 1 y 7: `)
-                  
                     if (ds==1){
                       console.log(diasemana[0])
                     }else if (ds==2){
@@ -223,11 +224,10 @@ function menu (){
             break;
             case "12":
                 function ejercicio12(){
+                  console.log("12.	Dado un mes en número, presentar el nombre de ese mes. Para la solución utilice arreglos.")
                     let meses=["Enero","Febrero","Marzo","Abril","Mayo","Junio","Julio","Agosto","Septiembre","Octubre","Noviembre","Diciembre"]
                     let numeromes=""
-                  
                     numeromes= read(`ingrese un numero compredido entre el 1 y 12: `)
-                  
                     if (numeromes==1){
                       console.log(meses[0])
                     }else if (numeromes==2){
@@ -255,46 +255,53 @@ function menu (){
                     }else{
                         console.log(`Mes no valido`)
                     }
-                  
                   }
                   ejercicio12()
             break;
             case "13":
                 function ejercicio13 (){
- 
+                  console.log("13.	Dado 5 nombres almacenarlo en un arreglo y luego presentar cada nombre del arreglo desde el ultimo al primero sin usar ciclos.")
                     let arrglonombre=[]
-                    
                      nombre = read ('ingrese un nombre:')
                      arrglonombre.push(nombre)
                      console.log(``)
-                   
                      nombre1 = read ('ingrese un nombre:')
                      arrglonombre.push(nombre1)
                      console.log(``)
-                   
                      nombre2 = read ('ingrese un nombre:')
                      arrglonombre.push(nombre2)
                      console.log(``)
-                   
                      nombre3 = read ('ingrese un nombre:')
                      arrglonombre.push(nombre3)
                      console.log(``)
-                   
                      nombre4 = read ('ingrese un nombre:')
                      arrglonombre.push(nombre4)
                      console.log(``)
                      console.log(arrglonombre)
                      console.log(``)
                      arrglonombre= arrglonombre.reverse()
-                     console.log(arrglonombre)
-                   
+                     console.log(arrglonombre) 
                    }
                    ejercicio13()
             break;
             case "14":
+                     function ejercicio14() {
+                      console.log("14.	Dado una dirección cualquiera presentar la dirección, el primer carácter, el del medio y el ultimo de dicha dirección")
+                       direccion= read (`Ingrese una direccion: `)
+                        let primerCaracter = direccion.charAt(0);
+                        let ultimoCaracter = direccion.charAt(direccion.length - 1);
+                        let posicionMedio = Math.floor(direccion.length / 2);
+                        let caracterMedio = direccion.charAt(posicionMedio)
+                        console.log("Dirección: " + direccion);
+                        console.log("Primer carácter: " + primerCaracter);
+                        console.log("Carácter del medio: " + caracterMedio);
+                        console.log("Último carácter: " + ultimoCaracter);
+                    }
+                    ejercicio14();
             break;
             case "15":
                 function ejercicio15(){
+                  console.log("15.	Almacenar 5 valores aleatorios en un arreglo e imprimir el primero valor si es par positivo y el ultimo si es impar negativo.")
                     let  valor = parseInt(Math.random()*100)
                     let valores=[]
                     valores.push(valor)
@@ -312,16 +319,14 @@ function menu (){
                     console.log(valores[0],valores[4])
                   }else{
                     console.log (valores[0],valores[4])
-                
                     
                   }
-                
-                 
                 }
                 ejercicio15()
             break;
             case "16":
                 function ejercicio16(){
+                  console.log("16.	Dado un arreglo vacío, añadir 3 nombres y presentar el primer y el ultimo carácter de cada nombre desde el arreglo.")
                     let arreglo=[]
                   let nombre="",nombre1="",nombre2=""
                   nombre = read ('ingrese un nombre:')
@@ -342,13 +347,48 @@ function menu (){
                 ejercicio16()
             break;
             case "17":
+                function ejercicio17(){
+                  console.log("17.	Dada una cadena presentar el primer carácter siempre y cuando sea un digito")
+                    console.log("17.	Dada una cadena presentar el primer carácter siempre y cuando sea un digito")
+                    console.log("")
+                    let cadena=""
+                    //,vocal1="a",vocal2="e",vocal3="i",vocal4="o",vocal5="u",vocal6="A",vocal7="E",vocal8="I",vocal9="O",vocal10="U"
+                    const numeros="0123456789"
+                    cadena= read(`Ingrese un caracter: `)
+                    console.log(``)
+                
+                    if (cadena[0]==numeros.charAt(0)){
+                      console.log(`${cadena[0]}...`)
+                    }else{
+                        console.log(`No se cumplen las condiciones...`)
+                    }
+                    
+                
+                }
+                ejercicio17()
             break;
             case "18":
+                function ejercicio18() {
+                  console.log("18.	Dada una cadena presentar el ultimo carácter siempre y cuando sea una letra")
+                    let cadena=""
+                    
+                    const vocales="bcdfghjklmnñpqrstvwxyzBCDFGHJLMNPRQSTVWXYZ"
+                    cadena= read(`Ingrese un caracter: `)
+                    console.log(``)
+                   
+                   
+                    if (cadena.charAt([cadena.length-1])==vocales.charAt([cadena.length-1])){
+                      console.log(`${cadena.charAt(cadena.length-1)}...`)
+                    }else{
+                        console.log(`No se cumplen las condiciones...`)
+                    }   
+                }
+                ejercicio18();
             break;
             case "19":
                 function ejercicio19(){
+                  console.log("19.	Dada una cadena presentar el primer carácter siempre y cuando sea una vocal")
                     let cadena=""
-                    //,vocal1="a",vocal2="e",vocal3="i",vocal4="o",vocal5="u",vocal6="A",vocal7="E",vocal8="I",vocal9="O",vocal10="U"
                     const vocales="aeiouAEIOU"
                     cadena= read(`Ingrese un caracter: `)
                     console.log(``)
@@ -364,9 +404,24 @@ function menu (){
                 ejercicio19()
             break;
             case "20":
+                function ejercicio20() {
+                  console.log("20.	Dada una cadena presentar el carácter de en medio, siempre y cuando sea un caracte de puntuación: “;” | “:” | “.” | “,”")
+                    let cadena=""
+                    const vocales=":;,."
+                    cadena= read(`Ingrese un caracter: `)
+                    console.log(``)
+                    if (cadena.charAt([cadena.length/2])==vocales.charAt([cadena.length/2])){
+                      console.log(`${cadena.charAt(cadena.length/2)}...`)
+                    }else{
+                        console.log(`No se cumplen las condiciones...`)
+                    }
+                }
+                ejercicio20();
+               
             break;
             case "21":
                 function ejercicio21(){
+                  console.log("21.	Dado dos caracteres indicar si son iguales o si el primero es menor que el segundo o mayor que el segundo")
                     let caracter1="",caracter2=""
                     caracter1= read(`Ingrese el primer caracter: `)
                     caracter2=read(`Ingrese el segundo caracter: `)
@@ -381,9 +436,8 @@ function menu (){
             break;
             case "22":
                 function ejercicio22 (){
+                  console.log("22.	Dado dos nombres indicar si son iguales o si el primero es menor que el segundo o mayor que el segundo")
                     let nombre=["ana","jose"]
-                  
-                
                     if (nombre[0]==nombre[1]){
                         console.log("Los nombres son iguales. ")
                     }else if(nombre[0]>nombre[1]){
@@ -397,6 +451,7 @@ function menu (){
             break;
             case "23":
                 function ejercicio23(){
+                  console.log("23.	Dado una cadena indicar cuantos elementos tiene, sin usar ciclos ")
                     let Cadena = "";
                     Cadena= read (`Ingrese la cadena: `)
                     let numeroCaracteres = Cadena.length;
@@ -406,7 +461,7 @@ function menu (){
             break;
             case "24":
                 function ejercicio24(){
-
+                  console.log("24.	Dado un arreglo indicar cuantos elementos tiene, sin usar ciclos")
                     let arreglo = [1, 2, 3, 4, 5], numeroElementos=0;
                     numeroElementos = arreglo.length;
                     console.log(`El arreglo tiene ${numeroElementos} elementos.`);
@@ -415,6 +470,7 @@ function menu (){
             break;
             default:
             console.log(`no existe ejercicio para el el número ingresado...`)
+            break;
 
 
 
